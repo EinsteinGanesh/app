6e9a5f42d70f67c7358da5917f04b514a717ccbe
EinsteinGanesh <mr.ganeshan23@gmail.com>
2023-03-30 22:47:44 +0530

    changes on view

--- action.js
+++ action.js
@@ -7,9 +7,9 @@ var subText = document.getElementsByClassName("submitted-text");
 function underline() {
     OngoingTest.style.borderBottom = "4px solid rgb(123,204,237)";
     CompletedTest.style.borderBottom = "4px solid rgb(255,255,255)";
-
+    OngoingTest.style.color="black"
     skillbox.style.display = "none";
-
+   
 
     for (var i = 0; i < bkmark.length; i++) {
         bkmark[i].style.display = "none";
@@ -18,12 +18,16 @@ function underline() {
 $(".submitted-text").html("Submit Before: 17th Aug 2022");
 $(".button").html("View Details");
 
+
+
 }
 
 function underline1() {
     OngoingTest.style.borderBottom = "4px solid rgb(255,255,255)";
     CompletedTest.style.borderBottom = "4px solid rgb(123,204,237)";
     skillbox.style.display = "block";
+
+  
     for (var i = 0; i < bkmark.length; i++) {
         bkmark[i].style.display = "block";
 
