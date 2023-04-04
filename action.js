a93daa4ccdf343ea36cb994d43023761de6e4377
EinsteinGanesh <mr.ganeshan23@gmail.com>
2023-04-04 19:26:53 +0530

    added model box

--- action.js
+++ action.js
@@ -41,3 +41,30 @@ function underline1() {
 function hello(){
     console.log("hello world")
 }
+
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.addEventListener("click", function(){
+    modal.style.display = "block";
+});
+  
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
